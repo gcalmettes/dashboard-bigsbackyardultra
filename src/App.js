20a0a362be20729806b3a6a79f12811a56f9b7e2
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2018-10-13 10:34:17 -0700

    react-router to handle navigation in navbar

--- src/App.js
+++ src/App.js
@@ -1,109 +1,45 @@
 import React, { Component } from 'react';
-import bigsData from './data/processData'
-import RaceTitle from './components/RaceTitle.js'
-import RaceRoster from './components/RaceRoster.js'
-import TimeLinesBrushable from './components/TimeLinesBrushable.js'
-import TimeLinesHoverable from './components/TimeLinesHoverable.js'
-import RunnersCumSumTime from './components/RunnersCumSumTime.js'
-import HistoTime from './components/HistoTime.js'
-import {Stack} from './components/subComponents/Stack.js'
-import {colorsSelected} from './components/subComponents/Colors.js'
+
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+
+import { BrowserRouter, HashRouter, Route, Switch, Link} from 'react-router-dom'
+
+import Dashboard from './components/Dashboard.js'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      hoveredBib: null,
-      brushedLaps: null,
-      selectedBibs: {}
+      variable: null
     }
-    
-    this.colorStack = new Stack(colorsSelected)
-
-    this.onHoverBib = this.onHoverBib.bind(this)
-    this.onBrushLaps = this.onBrushLaps.bind(this)
-    this.onClickBib = this.onClickBib.bind(this)
-
-  }
-
-  onHoverBib(bib){
-    this.setState({hoveredBib: bib})
-  }
-
-  onClickBib(bib){
-    if (this.state.selectedBibs[`${bib}`]) {
-      // put back color in stack
-      this.colorStack.push(this.state.selectedBibs[`${bib}`])
-      // filter out selected
-      const filtered = Object.keys(this.state.selectedBibs)
-        .filter(key => key !== `${bib}`)
-        .reduce((obj, key) => ({...obj, [key]: this.state.selectedBibs[key]}), {});
-      this.setState({selectedBibs: filtered})
-    } else {
-      const newSelection = {[`${bib}`]: this.colorStack.pop()}
-      this.setState({selectedBibs: {...newSelection, ...this.state.selectedBibs}})
-    }
-  }
-
-  onBrushLaps(lapsRange){
-    this.setState({brushedLaps: lapsRange})
   }
 
   render() {
     return (
-      <div>
-        <RaceTitle 
-          name = {bigsData.race}
-          date = {bigsData.time}
-        />
-        <RaceRoster 
-          runnersData={bigsData.data} 
-          hoveredBib = {this.state.hoveredBib} 
-          selectedBibs = {this.state.selectedBibs}
-          margins = {{top: 20, right: 20, bottom: 20, left: 20}}
-          width = {300}
-          height = {300}
-          onHover={this.onHoverBib}
-          onClick={this.onClickBib}
-        />
-        <RunnersCumSumTime
-          data = {bigsData.data}
-          margins = {{top: 20, right: 20, bottom: 20, left: 60}}
-          width = {350}
-          height = {300}
-          selectedBibs = {this.state.selectedBibs}
-        />
-        <div style= {{width: "700px"}}>
-          <TimeLinesHoverable 
-            data = {bigsData.data}
-            width = {700}
-            height = {250}
-            margins = {{top: 30, right: 50, bottom: 20, left: 50}}
-            hoveredBib = {this.state.hoveredBib}
-            selectedBibs = {this.state.selectedBibs}
-            xRange = {this.state.brushedLaps}
-          />
-          <TimeLinesBrushable
-            data = {bigsData.data}
-            width = {700}
-            height = {70}
-            margins = {{top: 0, right: 50, bottom: 35, left: 50}}
-            hoveredBib = {this.state.hoveredBib}
-            selectedBibs = {[]}
-            onBrush = {this.onBrushLaps}
-          />
+      <HashRouter>
+        <div>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>BBU</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/laz">Laz</Nav.Link>
+            </Nav>
+            <Navbar.Text>
+              Big Backyard Ultra 2017
+            </Navbar.Text>
+          </Navbar>
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/laz' component={()=> <div>Laz poetry</div>} />
+          </Switch>
         </div>
-        <HistoTime
-          data = {bigsData.data}
-          margins = {{top: 20, right: 20, bottom: 50, left: 60}}
-          width = {350}
-          height = {300}
-          selectedBibs = {this.state.selectedBibs}
-        />
-      </div>
-
-    );
+      </HashRouter>
+    )
   }
 }
 
 export default App;
+
---
+++ src/components/Dashboard.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+
+import bigsData from './../data/processData'
+import RaceRoster from './RaceRoster.js'
+import TimeLinesBrushable from './TimeLinesBrushable.js'
+import TimeLinesHoverable from './TimeLinesHoverable.js'
+import RunnersCumSumTime from './RunnersCumSumTime.js'
+import HistoTime from './HistoTime.js'
+import {Stack} from './subComponents/Stack.js'
+import {colorsSelected} from './subComponents/Colors.js'
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hoveredBib: null,
+      brushedLaps: null,
+      selectedBibs: {}
+    }
+    
+    this.colorStack = new Stack(colorsSelected)
+
+    this.onHoverBib = this.onHoverBib.bind(this)
+    this.onBrushLaps = this.onBrushLaps.bind(this)
+    this.onClickBib = this.onClickBib.bind(this)
+
+  }
+
+  onHoverBib(bib){
+    this.setState({hoveredBib: bib})
+  }
+
+  onClickBib(bib){
+    if (this.state.selectedBibs[`${bib}`]) {
+      // put back color in stack
+      this.colorStack.push(this.state.selectedBibs[`${bib}`])
+      // filter out selected
+      const filtered = Object.keys(this.state.selectedBibs)
+        .filter(key => key !== `${bib}`)
+        .reduce((obj, key) => ({...obj, [key]: this.state.selectedBibs[key]}), {});
+      this.setState({selectedBibs: filtered})
+    } else {
+      const newSelection = {[`${bib}`]: this.colorStack.pop()}
+      this.setState({selectedBibs: {...newSelection, ...this.state.selectedBibs}})
+    }
+  }
+
+  onBrushLaps(lapsRange){
+    this.setState({brushedLaps: lapsRange})
+  }
+
+  render() {
+    return (
+      <div>
+        <RaceRoster 
+          runnersData={bigsData.data} 
+          hoveredBib = {this.state.hoveredBib} 
+          selectedBibs = {this.state.selectedBibs}
+          margins = {{top: 20, right: 20, bottom: 20, left: 20}}
+          width = {300}
+          height = {300}
+          onHover={this.onHoverBib}
+          onClick={this.onClickBib}
+        />
+        <RunnersCumSumTime
+          data = {bigsData.data}
+          margins = {{top: 20, right: 20, bottom: 20, left: 60}}
+          width = {350}
+          height = {300}
+          selectedBibs = {this.state.selectedBibs}
+        />
+        <div style= {{width: "700px"}}>
+          <TimeLinesHoverable 
+            data = {bigsData.data}
+            width = {700}
+            height = {250}
+            margins = {{top: 30, right: 50, bottom: 20, left: 50}}
+            hoveredBib = {this.state.hoveredBib}
+            selectedBibs = {this.state.selectedBibs}
+            xRange = {this.state.brushedLaps}
+          />
+          <TimeLinesBrushable
+            data = {bigsData.data}
+            width = {700}
+            height = {70}
+            margins = {{top: 0, right: 50, bottom: 35, left: 50}}
+            hoveredBib = {this.state.hoveredBib}
+            selectedBibs = {[]}
+            onBrush = {this.onBrushLaps}
+          />
+        </div>
+        <HistoTime
+          data = {bigsData.data}
+          margins = {{top: 20, right: 20, bottom: 50, left: 60}}
+          width = {350}
+          height = {300}
+          selectedBibs = {this.state.selectedBibs}
+        />
+      </div>
+
+    );
+  }
+}
+
+export default Dashboard;
