23f41abe9e7524cf576b34452ec4e74af2459440
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2018-11-14 08:51:48 -0800

    Updates

---
+++ precache-manifest.51011404bce091b614301cc1a7e9c7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84204287038cf2eb3012",
+    "url": "/dashboard-bigsbackyardultra/static/js/runtime~main.84204287.js"
+  },
+  {
+    "revision": "6d46773c1c7feeaf7169",
+    "url": "/dashboard-bigsbackyardultra/static/js/main.6d46773c.chunk.js"
+  },
+  {
+    "revision": "a0517ba726fb9dc97b28",
+    "url": "/dashboard-bigsbackyardultra/static/js/1.a0517ba7.chunk.js"
+  },
+  {
+    "revision": "6d46773c1c7feeaf7169",
+    "url": "/dashboard-bigsbackyardultra/static/css/main.3b7a80e5.chunk.css"
+  },
+  {
+    "revision": "a0517ba726fb9dc97b28",
+    "url": "/dashboard-bigsbackyardultra/static/css/1.68f3dd4a.chunk.css"
+  },
+  {
+    "revision": "41ace1b00ef1e15c874522506b2d20cc",
+    "url": "/dashboard-bigsbackyardultra/index.html"
+  }
+];
